527e0e052563fe966b1e0537006a24ee5e79d88b
SahilMamedov <sahilim@code.edu.az>
2022-06-02 01:29:30 +0400

    Feat: Remember

--- assest/script.js
+++ assest/script.js
@@ -1,9 +1,10 @@
-let form=document.getElementById("form")
+let form=document.getElementById("form");
 let username=document.getElementById("username");
-let password=document.getElementById("password")
-let login=document.getElementById("login")
-let page=document.getElementById("page")
-let logout=document.getElementById("logout")
+let password=document.getElementById("password");
+let login=document.getElementById("login");
+let page=document.getElementById("page");
+let logout=document.getElementById("logout");
+let check=document.getElementById("checked")
 
 
 
@@ -11,7 +12,8 @@ let logout=document.getElementById("logout")
 const user={
     username:"sahil",
     password:"parol",
-    isLogin:false
+    isLogin:false,
+    
 }
 
 if(localStorage.getItem("Dbuser")==null){
@@ -40,15 +42,17 @@ Ischeked()
 
 login.addEventListener("click",function(ev){
     ev.preventDefault()
-    let check=document.getElementById("checked").checked;
-    if(check===true){
-        alert("true")
-    }
-    else if(check===false){
-        alert("false");
-    }
+
+    
 
     let user=JSON.parse(localStorage.getItem("Dbuser"))
+    
+    if(check.checked===true){
+        user.Ischeked=true;
+       
+    }
+    
+    
     
     if(user.username===username.value&&user.password===password.value){
        user.isLogin = true;
@@ -57,17 +61,32 @@ login.addEventListener("click",function(ev){
     }
     else{
         alert("Error.!!")
+        username.value="";
+        password.value="";
     }
     Ischeked()
 })
 
 logout.addEventListener("click",function(ev){
     ev.preventDefault()
+
+
+
+    
+    
+
+
     let user=JSON.parse(localStorage.getItem("Dbuser"))
+    if(user.Ischeked==true){
+        username.value=user.username
+        password.value=user.password
+    }
     user.isLogin = false
+    user.Ischeked=false
     localStorage.setItem("Dbuser",JSON.stringify(user))
-    username.value="";
-    password.value="";
+    // username.value="";
+    // password.value="";
+    
     Ischeked()
 })
 
